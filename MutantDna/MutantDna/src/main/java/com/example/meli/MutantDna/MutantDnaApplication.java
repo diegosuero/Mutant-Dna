a8d42b4ea002e81a743574d49ca6b0cb488dc759
diegouru <diegosuero1@gmail.com>
2022-02-15 18:31:12 -0300

    domain

---
+++ MutantDna/MutantDna/src/main/java/com/example/meli/MutantDna/MutantDnaApplication.java
@@ -0,0 +1,15 @@
+package com.example.meli.MutantDna;
+
+import com.example.meli.MutantDna.controller.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantDnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantDnaApplication.class, args);
+	}
+
+}
---
+++ MutantDna/MutantDna/src/main/java/com/example/meli/MutantDna/services/sequencer.java
@@ -0,0 +1,5 @@
+package com.example.meli.MutantDna.services;
+
+public class sequencer {
+    
+}
